3b67f4bede57b7cc12fc7faa0f946701f71feaf8
hisreal123 <omosgoerge@gmail.com>
2026-09-11 16:35:08 +0100

    fix: wrap app in ProviderScope

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pulse/features/expenses/expenses_list_page.dart';
 
 class App extends StatelessWidget {
-  const App ({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
